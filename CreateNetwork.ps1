dba3ec11f113e1ec9d52c3ce12ede350aa0edaa4
Alexander <sanyatishenko@outlook.com>
2018-10-25 18:44:10 +0000

    Init commit

---
+++ CreateNetwork.ps1
@@ -0,0 +1,52 @@
+﻿#Connect-AzureRmAccount
+
+# Variables for common values
+$resourceGroup = "ToDelResGroup"
+$location = "eastus"
+
+#Network
+$vnetAddressSpace = '10.10.10.0/24'
+$vnetName = 'ToDelVN'
+$subNetAddress = '10.10.10.0/25' 
+$subNetName = 'ToDelSubNet2'
+
+
+
+#*****************************#
+#                             #
+#    NETWORK CONFIGURATION    #
+#                             #
+#*****************************#
+
+
+
+$vnet = get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup -Name $vnetName -ErrorAction Ignore
+
+if (-not ($vnet))
+{
+    # Create a subnet configuration
+    $subnetConfig = New-AzureRmVirtualNetworkSubnetConfig `
+                    -Name $subNetName `
+                    -AddressPrefix $subNetAddress 
+    # Create a virtual network
+    $vnet = New-AzureRmVirtualNetwork `
+                    -ResourceGroupName $resourceGroup `
+                    -Location $location `
+                    -Name $vnetName `
+                    -AddressPrefix $vnetAddressSpace `
+                    -Subnet $subnetConfig
+} 
+else
+{
+    if (-not (Get-AzureRmVirtualNetworkSubnetConfig -Name $subNetName -VirtualNetwork $vnet -ErrorAction Ignore))
+    {
+        Add-AzureRmVirtualNetworkSubnetConfig `
+                        -Name $subNetName `
+                        -VirtualNetwork $vnet `
+                        -AddressPrefix $subNetAddress
+        Set-AzureRmVirtualNetwork `
+                        -VirtualNetwork $vnet
+     
+    }
+                  
+}
---
+++ CreateRG.ps1
@@ -0,0 +1,10 @@
+﻿# Create a resource group
+if (-not (get-AzureRmResourceGroup -Name $resourceGroup -ErrorAction Ignore))
+{
+    New-AzureRmResourceGroup -Name $resourceGroup -Location $location
+    Write-Host "Created resource group: $resourceGroup"
+} 
+else 
+{
+    Write-Host "Selected resource group: $resourceGroup"
+}
