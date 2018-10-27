79254b8d90b279582afe3bb9bcdc290e46551ca7
Alexander <sanyatishenko@outlook.com>
2018-10-27 20:10:00 +0000

    Add function Create-AzureNetwork

--- CreateNetwork.ps1
+++ CreateNetwork.ps1
@@ -1,52 +1,66 @@
 ﻿#Connect-AzureRmAccount
+Select-AzureRmSubscription -Subscription "8f87f15a-2f4b-4459-9410-315180be4dea"
 
-# Variables for common values
 $resourceGroup = "ToDelResGroup"
 $location = "eastus"
+$Path = "C:\Projects\AzureDevScripts\AzureNetwork.json"
+#############################################################################
+#                             
+#   Function create vNet and subnet in Azure    
+#   Requirement parameters: 
+#   $Path to the network configuration file
+#   $location Azure location
+#   $ResourceGroup Azure resource group name
+#
+#   Before runing function, you must runnin commands:
+#   Connect-AzureRmAccount
+#   Set-AzureRmSubscription
+#                             
+#
+################################################################################
 
-#Network
-$vnetAddressSpace = '10.10.10.0/24'
-$vnetName = 'ToDelVN'
-$subNetAddress = '10.10.10.0/25' 
-$subNetName = 'ToDelSubNet2'
+function Create-AzureNetwork {
+    Param(
+        [string]$Path,
+        [string]$location,
+        [string]$ResourceGroup
+    )
 
+    $content = Get-Content -Path $Path -Raw 
+    $NetJSON = ConvertFrom-Json $content
+    
+    $vnetName = $NetJSON.vNetName
+    $vnetAddressSpace = $NetJSON.vNetCIDR
+    
+    $vnet = get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup -Name $vnetName -ErrorAction Ignore
 
-
-#*****************************#
-#                             #
-#    NETWORK CONFIGURATION    #
-#                             #
-#*****************************#
-
-
-
-$vnet = get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup -Name $vnetName -ErrorAction Ignore
-
-if (-not ($vnet))
-{
-    # Create a subnet configuration
-    $subnetConfig = New-AzureRmVirtualNetworkSubnetConfig `
-                    -Name $subNetName `
-                    -AddressPrefix $subNetAddress 
-    # Create a virtual network
-    $vnet = New-AzureRmVirtualNetwork `
-                    -ResourceGroupName $resourceGroup `
-                    -Location $location `
-                    -Name $vnetName `
-                    -AddressPrefix $vnetAddressSpace `
-                    -Subnet $subnetConfig
-} 
-else
-{
-    if (-not (Get-AzureRmVirtualNetworkSubnetConfig -Name $subNetName -VirtualNetwork $vnet -ErrorAction Ignore))
-    {
-        Add-AzureRmVirtualNetworkSubnetConfig `
-                        -Name $subNetName `
-                        -VirtualNetwork $vnet `
-                        -AddressPrefix $subNetAddress
-        Set-AzureRmVirtualNetwork `
-                        -VirtualNetwork $vnet
+    if (-not ($vnet)){
+            # Create a virtual network
+            $vnet = New-AzureRmVirtualNetwork `
+                            -ResourceGroupName $resourceGroup `
+                            -Location $location `
+                            -Name $vnetName `
+                            -AddressPrefix $vnetAddressSpace                         
+        } 
+    else {
+            Write-Host "vNet $vnetName alredy exist" -ForegroundColor DarkMagenta
+        } #End If vNet Create
+    
+    foreach ($SubNet in $NetJSON.vNetSubnets) {
+        if (-not (Get-AzureRmVirtualNetworkSubnetConfig -Name $SubNet.SubNetName -VirtualNetwork $vnet -ErrorAction Ignore)){
+            Write-Host "Create SubNet $($SubNet.SubNetName)" -ForegroundColor DarkMagenta
+            Add-AzureRmVirtualNetworkSubnetConfig `
+                            -Name $SubNet.SubNetName `
+                            -VirtualNetwork $vnet `
+                            -AddressPrefix $SubNet.SubNetCIDR
+            Set-AzureRmVirtualNetwork `
+                            -VirtualNetwork $vnet
      
-    }
-                  
-}
+        } 
+        else {
+                Write-Host "SubNet $($SubNet.SubNetName) alredy exist" -ForegroundColor DarkMagenta
+            }
+    } # End ForEach vNetSubnets
+} #End Function Create-AzureNetwork
+
+Create-AzureNetwork -Path $Path -ResourceGroup $resourceGroup -location $location
