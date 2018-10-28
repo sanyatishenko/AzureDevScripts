8e31bbe9136e200191b76d4935df4b55a12a339b
Alexander <sanyatishenko@outlook.com>
2018-10-28 09:29:59 +0000

    Add function Create-ResourceGroup

--- CreateRG.ps1
+++ CreateRG.ps1
@@ -1,10 +1,30 @@
-﻿# Create a resource group
-if (-not (get-AzureRmResourceGroup -Name $resourceGroup -ErrorAction Ignore))
-{
-    New-AzureRmResourceGroup -Name $resourceGroup -Location $location
-    Write-Host "Created resource group: $resourceGroup"
-} 
-else 
-{
-    Write-Host "Selected resource group: $resourceGroup"
+﻿##########################################################
+#
+#   Function create Resource Group in Azure
+#   Requirement parameters: 
+#   $location Azure location
+#   $ResourceGroup Azure resource group name
+#
+#   Before runing function, you must runnin commands:
+#   Connect-AzureRmAccount
+#   Set-AzureRmSubscription
+#
+##########################################################
+function Create-ResourceGroup {
+    param (
+        [string]$resourceGroup,
+        [string]$location
+    )
+    # Create a resource group
+    if (-not (get-AzureRmResourceGroup -Name $resourceGroup -ErrorAction Ignore))
+    {
+        New-AzureRmResourceGroup -Name $resourceGroup -Location $location
+        Write-Host "Created resource group: $resourceGroup"
+    } 
+    else 
+    {
+        Write-Host "Selected resource group: $resourceGroup"
+    }
+
+
 }
